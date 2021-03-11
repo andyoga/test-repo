ed02cb03218f506746ed35a36cd48afc36add864
Andhika Yoga Adhyatma <andhika.yoga@outlook.com>
2021-03-11 05:50:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/ 
 echo "\
 [general]\n\
-email = \"{andhika.yoga@outlook.com}\"\n\
+email = \"andhika.yoga@outlook.com\"\n\
 " > ~/.streamlit/credentials.toml 
 echo "\
 [server]\n\
