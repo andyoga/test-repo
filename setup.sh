26309ba69f3e1e7d8a9ea520f5409226918c1425
Andhika Yoga Adhyatma <andhika.yoga@outlook.com>
2021-03-11 06:26:34 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 mkdir -p ~/.streamlit/ 
-echo "\
-[general]\n\
-email = \"andhika.yoga@outlook.com\"\n\
-" > ~/.streamlit/credentials.toml 
+
 echo "\
 [server]\n\
+port = $PORT\n\
+enableCORS=false\n\
 headless = true\n\
-enableCORS=false\n\port = $PORT\n\
+\n\
 " > ~/.streamlit/config.toml
